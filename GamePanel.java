018da65ae5a064d488acea4dd773d3f195475c25
djmoin <134087164+djmoin@users.noreply.github.com>
2023-05-29 22:05:23 +0600

    GamePanel.java

--- GamePanel.java
+++ GamePanel.java
@@ -40,5 +40,10 @@ public class GamePanel extends JPanel implements Runnable{
 	}
 	public void paintCOmponent(Graphics g){
 		super.paintComponent(g);
+		Graphics2D g2 = (Graphics2D)g;
+		
+		g2.setColor(Color.white);
+		g2.fillRect(100,100,tileSize,tileSize);
+		g2.dispose();
 	}
 }
