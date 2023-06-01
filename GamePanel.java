852f804470b510ce6521681025a55301625e6208
djmoin <134087164+djmoin@users.noreply.github.com>
2023-06-01 22:44:46 +0600

    GamePanel.java

--- GamePanel.java
+++ GamePanel.java
@@ -18,10 +18,18 @@ public class GamePanel extends JPanel implements Runnable{
 	final int ScreenWidth = tileSize*maxScreenCol;	//768 pixels
 	final int ScreenHeight = tileSize*maxScreenRow;	//576 pixels
 	
+	//FPS
+	
+	int FPS = 60;
+	
 	KeyHandler KH = new KeyHandler();
 	
-	
 	Thread gameThread;
+	
+	//Player Position 
+	int playerX=100;
+	int playerY=100;
+	int playerSpeed=5;
 	
 	public GamePanel() {
 		this.setPreferredSize(new Dimension(ScreenWidth, ScreenHeight));
@@ -38,20 +46,58 @@ public class GamePanel extends JPanel implements Runnable{
 	
 	@Override
 	public void run() {
+		
+		double drawInterval = 1000000000/FPS;
+		double nextDrawTime = System.nanoTime()+drawInterval;
 		while(gameThread!=null) {
+			
+			long currentTime=System.nanoTime();
+			System.out.println("Current time :"+currentTime);
+			
 			update();
 			
 			repaint();
+			
+			
+			try {
+				double remainingTime = nextDrawTime-System.nanoTime();
+				remainingTime=remainingTime/1000000;
+				
+				if(remainingTime<0) {
+					remainingTime=0;
+				}
+				
+				Thread.sleep((long) remainingTime);
+				
+				nextDrawTime += drawInterval;
+				
+			} catch(InterruptedException e) {
+				e.printStackTrace();
+			}
+			
+			
 		}
 	}
 	public void update(){
+		if(KH.upPrssd==true) {
+			playerY-=playerSpeed;
+		}
+		else if(KH.downPrssd==true) {
+			playerY+=playerSpeed;
+		}
+		else if(KH.rightPrssd==true) {
+			playerX+=playerSpeed;
+		}
+		else if(KH.leftPrssd==true) {
+			playerX-=playerSpeed;
+		}
 	}
 	public void paintComponent(Graphics g){
 		super.paintComponent(g);
 		Graphics2D g2 = (Graphics2D)g;
 		
 		g2.setColor(Color.white);
-		g2.fillRect(100,100,tileSize,tileSize);
+		g2.fillRect(playerX,playerY,tileSize,tileSize);
 		g2.dispose();
 	}
 }
